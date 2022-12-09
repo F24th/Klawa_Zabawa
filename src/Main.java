d0e37e136e878da7fd2d84b0c75be79ab0932e3f
F24th <116838181+F24th@users.noreply.github.com>
2022-12-09 22:07:20 +0100

    1.0.2

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,28 @@
+
 import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++)
-        {
-            int m1 = 0;
-            int m2 = 5;
-            int random;
-            Random rand = new Random();
-            random = rand.nextInt(m2 - m1) + m1;
-            lista_slow l = new lista_slow();
-            System.out.println(l.hard[random]);
-        }
+//deklaracje zmiennych
+        String slowo;
+        int m1 = 0;
+        int m2 = 5;
+        int random;
+
+//wylosowanie liczby z przedzialu <m1,m2>
+        Random rand = new Random();
+        random = rand.nextInt(m2 - m1) + m1;
+
+//przypisanie do zmiennej slowo losowego slowa z listy
+        lista_slow l = new lista_slow();
+        slowo = l.easy[random];
+        System.out.println(slowo);
+
+//podzielenie słowa na litery
+        String[] podzielone = slowo.split("");
+        System.out.println(podzielone[1]);
+
+
+
     }
     }
